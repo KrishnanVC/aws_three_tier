e54eec859a6a3702201b0344ee81f20abc4111d2
Krishnan VC <krishnan.vcg@gmai.com>
2024-02-06 19:12:52 +0530

    Add s3 backend

--- provider.tf
+++ provider.tf
@@ -5,4 +5,11 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket  = "terraform-aws-three-tier-state"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
 }
